a65ca50310efd4f3bb85a26635c29a3ce625a1d1
Mehdi Teabani <77509689+mehdi-eng@users.noreply.github.com>
2023-08-23 13:11:52 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+print("Simple Calculator App")
+print("Operations: +, -, *, /")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == "+":
+    result = add(num1, num2)
+elif operator == "-":
+    result = subtract(num1, num2)
+elif operator == "*":
+    result = multiply(num1, num2)
+elif operator == "/":
+    result = divide(num1, num2)
+else:
+    result = "Invalid operator"
+
+print("The result is:", result)
